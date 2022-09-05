e65da21db8839d20908fa4210c2dbcaf4eb42ae2
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-05 16:22:48 +0100

    updated from the feedback

--- Controllers/reviews.controller.js
+++ Controllers/reviews.controller.js
@@ -4,6 +4,7 @@ exports.getReview = (req, res, next) => {
   const param = req.params;
   selectReviewsById(param.review_id)
     .then((reviewById) => {
+      console.log(reviewById);
       res.status(200).send({ game: reviewById });
     })
     .catch((err) => {
