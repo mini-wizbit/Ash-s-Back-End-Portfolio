86900c6ae0d08ce13fcfc1035f7777be446b9430
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-06 10:42:43 +0100

    fixed conflict and all current tests are passing :)

--- Model/review.model.js
+++ Model/review.model.js
@@ -1,13 +1,17 @@
 const db = require("../db/connection.js");
 
 exports.selectReviewsById = (review_id) => {
-  return db
-    .query("SELECT * FROM reviews WHERE review_id = $1", [review_id])
-    .then((reviewOfIdNumber) => {
-      if (reviewOfIdNumber.rows.length === 0) {
-        return Promise.reject({ status: 400, msg: "Bad Request" });
-      } else {
-        return reviewOfIdNumber.rows[0];
-      }
-    });
+  if (review_id > 0) {
+    return db
+      .query("SELECT * FROM reviews WHERE review_id = $1", [review_id])
+      .then((reviewOfIdNumber) => {
+        if (reviewOfIdNumber.rows.length === 0) {
+          return Promise.reject({ status: 404, msg: "Not Found" });
+        } else {
+          return reviewOfIdNumber.rows[0];
+        }
+      });
+  } else {
+    return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
 };
