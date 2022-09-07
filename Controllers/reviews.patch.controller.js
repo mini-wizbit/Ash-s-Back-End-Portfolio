3026b389be0812963f1ff977df35b78aa2e44b7f
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-07 09:31:27 +0100

    naming convention post -> patch because ash is silly

--- Controllers/reviews.patch.controller.js
+++ Controllers/reviews.patch.controller.js
@@ -1,9 +1,11 @@
-const { selectReviewPost } = require("../Model/reviews.patch.model");
+const {
+  selectReviewPost: selectReviewPatch,
+} = require("../Model/reviews.patch.model");
 
 exports.patchReview = (req, res, next) => {
   const review_id = req.params;
   const patchInfo = req.body;
-  selectReviewPost(review_id, patchInfo)
+  selectReviewPatch(review_id, patchInfo)
     .then((review) => {
       res.status(200).send({ review });
     })
