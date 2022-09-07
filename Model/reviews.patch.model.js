0c7023239f7e73457be578f642107f921971a5b0
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-07 12:09:22 +0100

    refactored w/Chipie NC as I kept falling back into what i did previously

--- Model/reviews.patch.model.js
+++ Model/reviews.patch.model.js
@@ -2,48 +2,24 @@ const db = require("../db/connection.js");
 
 exports.selectReviewPost = (review_id, patchInfo) => {
   const id = review_id.reviews_id;
-  const toPatchThisKey = Object.keys(patchInfo);
-  const validReviewKeys = [
-    "title",
-    "review_body",
-    "designer",
-    "review_img_url",
-    "votes",
-    "category",
-    "owner",
-  ];
+  const toPatchThisKey = Object.keys(patchInfo)[0];
+  const newValue = patchInfo[toPatchThisKey];
+
+  let update = "";
+  if (typeof newValue === "number") {
+    update = `${toPatchThisKey} + $1`;
+  } else {
+    update = "$1";
+  }
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = ${id};`)
-    .then((reviewsIdData) => {
-      /*if (reviewsIdData.rows.length === 0) {
+    .query(
+      `UPDATE reviews SET ${toPatchThisKey} = ${update} WHERE review_id = $2 RETURNING *`,
+      [newValue, id]
+    )
+    .then((results) => {
+      if (results.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not Found" });
       }
-      let currentValue = reviewsIdData.rows[0][toPatchThisKey[0]];
-      let queryStr = "UPDATE reviews";
-      let newValue = 0;
-      const createdQueryObj = {};
-      if (
-        typeof patchInfo[toPatchThisKey[0]] === "number" &&
-        toPatchThisKey[0] === "votes"
-      ) {
-        newValue = currentValue += patchInfo[toPatchThisKey[0]];
-      } else {
-        return Promise.reject({ status: 400, msg: "Bad Request" });
-      }
-      if (validReviewKeys.includes(toPatchThisKey[0])) {
-        queryStr += ` SET ${toPatchThisKey[0]} = $1`;
-        queryStr += ` WHERE review_id = $2 RETURNING *;`;
-      }
-      createdQueryObj.string = queryStr;
-      createdQueryObj.value = newValue;
-      createdQueryObj.id = id;
-      return createdQueryObj;
-      */
+      return results.rows[0];
     });
-  //     .then((queryStr) => {
-  //       return db.query(queryStr.string, [queryStr.value, queryStr.id]);
-  //     })
-  //     .then((results) => {
-  //       return results.rows[0];
-  //     });
 };
