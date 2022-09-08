d91ffaa7c5e24915098e16894c1764623e4a9776
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-08 12:04:29 +0100

    one query and a new test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -297,7 +297,7 @@ describe("Using app.js to run the database of NC-games", () => {
             "created_at",
             "2021-01-18T10:01:41.251Z"
           );
-          expect(body.game).toHaveProperty("comment_count", 3);
+          expect(body.game).toHaveProperty("comment_count", "3");
         });
     });
     test("404: Not Found from possible spelling mistake", () => {
@@ -306,6 +306,29 @@ describe("Using app.js to run the database of NC-games", () => {
         .expect(404)
         .then((response) => {
           expect(response.body).toEqual({ msg: "Not Found" });
+        });
+    });
+    test("200: responds with a single matching review NOW with comment count at 0/nothing", () => {
+      const review_id = 7;
+      return request(app)
+        .get(`/api/reviews/${review_id}`)
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          expect(body.game.votes).toBe(9);
+          expect(body.game).toHaveProperty("review_id", 7);
+          expect(body.game).toHaveProperty(
+            "title",
+            "Mollit elit qui incididunt veniam occaecat cupidatat"
+          );
+          expect(body.game).toHaveProperty("category", "social deduction");
+          expect(body.game).toHaveProperty("designer", "Avery Wunzboogerz");
+          expect(body.game).toHaveProperty("owner", "mallionaire");
+          expect(body.game).toHaveProperty(
+            "created_at",
+            "2021-01-25T11:16:54.963Z"
+          );
+          expect(body.game).toHaveProperty("comment_count", "0");
         });
     });
   });
